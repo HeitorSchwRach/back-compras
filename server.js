19fa174850da30e939e0105e19f2620ca8c7dfb0
HEITOR SCHWINDEN RACHADEL <heitor_rachadel@estudante.sesisenai.org.br>
2025-11-04 11:14:31 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import { log } from 'console';
+import Fastify from 'fastify';
+import pkg from 'pg'
+
+const{Pool} = pkg
+
+const pool = new Pool({
+    user: 'local',
+    host: 'localhost',
+    database: 'compras',
+    password: '12345',
+    port: 5432
+})
+
+const server = Fastify()
+
+server.get('/compras', async (req, reply)=>{
+    const result = await pool.query(`SELECT * FROM purchase`)
+    return result.rows
+})
+
+server.post('/compras', async(req, reply)=>{ 
+    const body = req.body
+
+    console.log(
+        body
+    );
+    
+    const result = await pool.query(`
+        INSERT INTO purchase (name, price, description) 
+        VALUES ($1, $2, $3) RETURNING *
+    `, [body.name, body.price, body.description ])
+    
+    return {response:'Deu bom!'}
+
+    return {response: result.rows}
+})
+
+server.listen({port:3333})
